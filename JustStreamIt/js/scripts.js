4a3a33aa5141cbcb9c9190ca7702a1e8d8b55778
johnchem <j.payssan@gmail.com>
2022-06-09 11:47:19 +0200

    test et layout HTML

---
+++ JustStreamIt/js/scripts.js
@@ -0,0 +1,16 @@
+/* let var1 = 42;
+const salutation = "C'est le ${var1} hello world !";
+console.log(salutation);
+
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+}
+*/
+
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'JustStreamIt';
+
