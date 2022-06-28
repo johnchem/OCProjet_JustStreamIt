21ed471c4c178e7e71e524c300c2e3d19ba04d6d
johnchem <j.payssan@gmail.com>
2022-06-28 22:21:25 +0200

    test parcel & donnée json

--- JustStreamIt/js/scripts.js
+++ JustStreamIt/js/scripts.js
@@ -11,10 +11,46 @@ class Book {
 }
 */
 
+
+var json_data =
+{
+    "tutu": {
+        "id": 9,
+        "url": "http://127.0.0.1:8000/api/v1/titles/9",
+        "title": "Miss Jerry"
+    },
+    "toto": {
+        "id": 574,
+        "url": "http://127.0.0.1:8000/api/v1/titles/574",
+        "title": "The Story of the Kelly Gang"
+    }
+};
+
+/*
+var json_file = '{
+"id": 9,
+    "url": "http://127.0.0.1:8000/api/v1/titles/9",
+        "title": "Miss Jerry"}';
+*/
+var data = JSON.parse(json_data);
+
+console.log(data.tutu);
+
+/*
+const data = (function () {
+    const json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': "../mock_data.json",
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json
+})();
+*/
+
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'JustStreamIt';
-
-document
-    .getElementById("bestScoredMovie")
-    .getElementsByClassName("card-title")
-    .innerHTML = ["test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8"];
